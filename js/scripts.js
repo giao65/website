992e575caedbd7c70743fc64bccde3dc6efabcf9
giao65 <starfall5571920@gmail.com>
2021-08-03 14:41:28 +0800

    remote_test

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ function submit() {
       content,
     };
 
-    postData("http://127.0.0.1:5000/predict", data).then((result) => {
+    postData("http://ef3c16eee5a8.ngrok.io/predict", data).then((result) => {
       console.log(result);
       document.getElementById("resultText").innerHTML = predictType(result);
     });
