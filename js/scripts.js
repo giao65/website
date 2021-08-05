764fc9b8e0f6536e2d16b9149938027e061d5a98
giao65 <starfall5571920@gmail.com>
2021-08-05 15:32:38 +0800

    local_test

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,7 @@ function submit() {
       content,
     };
 
-    postData("http://7ced91277b2f.ngrok.io/predict", data).then((result) => {
+    postData("http://521a9c442363.ngrok.io/predict", data).then((result) => {
       console.log(result);
       document.getElementById("resultText").innerHTML = predictType(result);
     });
